5b856af047d538c59bd374fc7f9981139bcec6e6
Janshen Vernon <janshen.vbb@gmail.com>
2022-06-27 02:45:35 -0300

    created migration for creating Produto table

--- src/seeders/migrations.js
+++ src/seeders/migrations.js
@@ -1,1 +1,16 @@
 const schemas = require('../database/schemas');
+const knex = require('../database/connection');
+
+async function RunMigration() {
+    try {
+        const existingTableValues = await knex('produtos')
+            .select('*');
+
+        if (existingTableValues.length) return;
+    } catch (error) {
+        const executingSchemas = await knex.raw(schemas);
+        return;
+    };
+};
+
+module.exports = RunMigration;
