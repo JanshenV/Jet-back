8e21e1b6b8b9b9f7331bce4a743cd217ae069361
Janshen Vernon <janshen.vbb@gmail.com>
2022-06-27 02:46:45 -0300

    called migration seeder into the index file for when the project is initialized for the first time

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const app = require('./server');
 
 //Seeders
-
+const RunMigration = require('./seeders/migrations');
+RunMigration();
 
 app.listen(process.env.PORT || 80);
 
